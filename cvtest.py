084271cfcd41db8c60172aa71527c888a94399ef
Jesse <pango3001@gmail.com>
2020-11-14 22:56:58 +0000

    added servo controlling and tracing for y axis

--- cvtest.py
+++ cvtest.py
@@ -1,23 +1,62 @@
 import cv2
 import numpy as np
 import time
-import imutils
-from collections import deque
+import Adafruit_PCA9685
 
+#servo driver board
+pwm = Adafruit_PCA9685.PCA9685()
 
+#servo stuff
+pwm.set_pwm_freq(60)
+servo_min = 150  # Min pulse length out of 4096
+servo_max = 600  # Max pulse length out of 4096
+
+#raspi camera
 cap = cv2.VideoCapture(0)
 
+#set capture resolution (smaller for sake of speed)
+cap.set(3,480)
+cap.set(4,320)
 _, frame = cap.read()
 rows, cols, _ = frame.shape
 
 x_medium = int(cols / 2)
 y_medium = int(rows / 2)
-center = int(cols / 2)
-position = 90 # degrees
+x_center = int(cols / 2)
+y_center = int(rows / 2)
+x_position = int((600-150)/2) # starting x position 150 low     600 high
+y_position = int(480) # starting y position 150 low     600 high
+buffer = 50 #pixels
+
+#center servos
+pwm.set_pwm(0, 0, x_position)
+pwm.set_pwm(2, 0, y_position)
+
+
 
 def nothing(x):
     pass
 
+def x_posit(x):
+    global x_position
+    x_position += x
+    if x_position > servo_max:
+        x_position = servo_max
+    if x_position < servo_min:
+        x_position = servo_min
+    pwm.set_pwm(0, 0, x_position)
+
+def y_posit(x):
+    global y_position
+    y_position += x
+    if y_position > servo_max:
+        y_position = servo_max
+    if y_position < servo_min:
+        y_position = servo_min
+    pwm.set_pwm(2, 0, y_position)
+
+
+#HSV Control Window   initially set to track red
 cv2.namedWindow("Control")
 cv2.createTrackbar("LH", "Control", 150, 255, nothing)
 cv2.createTrackbar("LS", "Control", 155, 255, nothing)
@@ -45,36 +84,54 @@ while True:
     mask = cv2.inRange(hsv, low_red, high_red)
     mask = cv2.erode(mask, None, iterations=2)
     mask = cv2.dilate(mask, None, iterations=2)
+    
     res = cv2.bitwise_and(frame, frame, mask=mask)
+    
     contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     contours = sorted(contours, key=lambda x:cv2.contourArea(x), reverse=True)
     
-    
+    # tracking
     for cnt in contours:
         (x, y, w, h) = cv2.boundingRect(cnt)
-        
         x_medium = int((x + x + w) / 2)
         break 
     for cnt in contours:
         (x, y, w, h) = cv2.boundingRect(cnt)
-        
         y_medium = int((y + y + h) / 2)
         break
     
+    #move x axis servo
+    if x_medium < x_center - buffer:
+        x_posit(3)
+    elif x_medium > x_center + buffer:
+        x_posit(-3)
+        
+    #move y axis servo    
+    if y_medium < y_center - buffer:
+        y_posit(-3)
+    elif y_medium > y_center + buffer:
+        y_posit(3)
+        
+        
+    
+    #buffer lines
+    cv2.line(frame, (x_center - buffer,0), (x_center - buffer, 480), (0, 255, 255), 2)
+    cv2.line(frame, (x_center + buffer,0), (x_center + buffer, 480), (0, 255, 255), 2)
+    cv2.line(frame, (0, y_center - buffer), (640, y_center - buffer), (0, 255, 255), 2)
+    cv2.line(frame, (0, y_center + buffer), (640, y_center + buffer), (0, 255, 255), 2)
+    
+    
+    #tracking ling
     cv2.line(frame, (x_medium, 0), (x_medium, 480), (0, 255, 0), 2)
     cv2.line(frame, (0, y_medium), (640, y_medium), (0, 255, 0), 2)
     cv2.line(mask, (x_medium, 0), (x_medium, 480), (0, 255, 0), 1)
     cv2.line(mask, (0, y_medium), (640, y_medium), (0, 0, 255), 1)
     
-    
+    #GUI Windows of video feed
     cv2.imshow("Frame", frame)
-
     cv2.imshow("Mask", mask)
-    cv2.imshow("Res", res)
+    #cv2.imshow("Res", res)
     key = cv2.waitKey(1)
-    
-    
-
     
     if key == 27:   # esc key
         break
